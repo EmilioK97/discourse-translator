f3cb4913238fc6ffb440a5951933c2c17d243a6d
EmilioK <emiliovirtual@hotmail.com>
2017-10-14 18:07:17 +0200

    Fix

--- services/discourse_translator/deepl.rb
+++ services/discourse_translator/deepl.rb
@@ -6,6 +6,7 @@ module DiscourseTranslator
     TRANSLATE_URI = "https://www.deepl.com/jsonrpc".freeze
 
     SUPPORTED_LANG = {
+      auto: 'auto'
       en: 'EN',
       de: 'DE',
       es: 'ES',
